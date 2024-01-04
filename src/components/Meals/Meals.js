83106b8ad56370b8a6f09fa4e4e62deb80b7f344
dreamyharshita <dreamjob9893@gmail.com>
2024-01-04 14:29:34 +0530

    meals summary added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailaibleMeals from "./AvailaibleMeals";
+import { Fragment } from "react";
+
+const Meals=()=>{
+return <Fragment>
+    <MealsSummary/>
+    <AvailaibleMeals/>
+</Fragment>
+
+}
+
+export default Meals;
